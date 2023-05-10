472c9d097f19c68e6fdc10684ed3da48f75c96f2
aniketp080 <aniketpatil85@yahoo.com>
2023-05-10 10:50:30 +0530

    random changes

--- src/component/Contact.js
+++ src/component/Contact.js
@@ -25,23 +25,23 @@ const Contact = () => {
             <ul style={{listStyle:"none", lineHeight:"40px"}}>
 
                 <li>
-                <p className="card-text text-muted"  style={{fontSize:"17px"}}><i class="fa-solid fa-envelope" style={{color: "#f7f7f7"}}></i>--aniketpatil85@yahoo.com</p>
+                <p className="card-text text-muted"  style={{fontSize:"17px"}}><i className="fa-solid fa-envelope" style={{color: "#f7f7f7"}}></i>--aniketpatil85@yahoo.com</p>
 
                 </li>
                 <li>
-                <p className="card-text text-muted" style={{fontSize:"17px"}}><i class="fa-solid fa-address-book" style={{color: "#f2f2f2"}}></i>--9702319663/9325699946</p>
+                <p className="card-text text-muted" style={{fontSize:"17px"}}><i className="fa-solid fa-address-book" style={{color: "#f2f2f2"}}></i>--9702319663/9325699946</p>
 
                 </li>
                 <li>
-                <p className="card-text text-muted" style={{fontSize:"17px"}}> <i class="fa-sharp fa-solid fa-location-dot" style={{color:"#d0d6e2"}}></i> Sion,Chunabhatti Mumbai-400022</p>
+                <p className="card-text text-muted" style={{fontSize:"17px"}}> <i className="fa-sharp fa-solid fa-location-dot" style={{color:"#d0d6e2"}}></i> Sion,Chunabhatti Mumbai-400022</p>
                 </li>
             </ul>
             
                 <div className='socialIcons'>
-                <a href=""><i class="fa-brands fa-github fa-lg" ></i></a>
-                <a href=""><i class="fa-brands fa-instagram fa-lg" ></i></a>
-                <a href=""><i class="fa-brands fa-linkedin fa-lg" ></i></a>
-                <a href=""><i class="fa-brands fa-whatsapp fa-lg"></i></a>
+                <a href=""><i className="fa-brands fa-github fa-lg" ></i></a>
+                <a href=""><i className="fa-brands fa-instagram fa-lg" ></i></a>
+                <a href=""><i className="fa-brands fa-linkedin fa-lg" ></i></a>
+                <a href=""><i className="fa-brands fa-whatsapp fa-lg"></i></a>
                 </div>
 
                 
--- src/component/Navbar.js
+++ src/component/Navbar.js
@@ -9,7 +9,7 @@ const Navbar = () => {
             <nav className="navbar navbar-expand-lg navbar-dark bg-dark">
                 <div className="container-fluid" >
              
-                    <a className="navbar-brand name"  data-text="Aniket Patil" href="#" style={{fontSize:"24px",fontWeight:"bold",color:"black"}}>Aniket Patil</a>
+                    <a className="navbar-brand name"  data-text="Aniket Patil " href="#" style={{fontSize:"24px",fontWeight:"bold",color:"black"}}>Aniket Patil </a>
                     <button className="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
                     <span className="navbar-toggler-icon"></span>
                     </button>
